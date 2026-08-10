db491e8658f19cdee322693e2398718244b6c03f
Thomas Whiteside <thomasw>
2026-08-10 09:04:31 +0100

    Added Routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Budget } from './budget/budget';
+import { Accounts } from './accounts/accounts';
+import { Transactions } from './transactions/transactions';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'budget', component: Budget},
+    {path: 'accounts', component: Accounts},
+    {path: 'transactions', component: Transactions},
+];
