2bd650a059d8c36c96135e03cac492065f97d584
ryan sheng <ryansheng@proton.me>
2026-08-11 04:03:50 -0700

    hope1

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,10 +1,11 @@
 
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL
 });
 
 export const prisma = new PrismaClient({ adapter });
+
