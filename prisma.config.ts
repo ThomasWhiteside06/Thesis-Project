411e4b7b393bb6e63feced0ea14f15cc04ebefba
ryan sheng <ryansheng@proton.me>
2026-08-12 00:45:28 -0700

    added accounts_1

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,11 @@
+// 
+
+
 import { defineConfig } from "@prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
   datasource: {
-    db: {
-      provider: "postgresql",
-      url: process.env.DATABASE_URL,
-    },
-  },
+    provider: "postgresql",
+    url: process.env.DATABASE_URL
+  }
 });
